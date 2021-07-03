24d4395ad16b837bd25dfc31624a9fa985a2293b
tgrzywacz <tomasz.grzywacz@altkomsoftware.pl>
2021-07-03 16:16:51 +0200

    17. Walidacja zapytania

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -54,6 +54,11 @@ namespace dotnetWebApi.Controllers
         [HttpPost]
         public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             var restaurant = _mapper.Map<Restaurant>(dto);
 
             _dbContext.Restaurants.Add(restaurant);
